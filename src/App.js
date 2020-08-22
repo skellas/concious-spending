022cfcd26b42ca27001744e5c0f7f348f6053476
Seth Kellas <sjkellas@gmail.com>
2020-08-22 18:49:25 -0400

    Initial CRA setup and cleanup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Concious Spending Plan</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders header', () => {
+  const { getByText } = render(<App />);
+  const header = getByText(/Concious Spending/);
+  expect(header).toBeInTheDocument();
+});
