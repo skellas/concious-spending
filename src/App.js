4ba3bbf31fc04beeead6c1e4b0751c32b3288bff
Seth Kellas <sjkellas@gmail.com>
2020-08-23 15:16:09 -0400

    Pass in income. Add more colspan.

--- src/App.js
+++ src/App.js
@@ -33,23 +33,27 @@ function App() {
 
         <Grid item>
           <Grid container direction='row' spacing={4}>
-            <Grid item xs={9}>
+            <Grid item xs={12}>
               <TextField fullWidth id='income' label='My take home income is'
                 value={income} onChange={handleIncomeChange}
                />
             </Grid>
           </Grid>
           <PercentageInput id='savings-slider' label='I want to save' 
-            minimumValue={0} maximumValue={50} defaultValue={10} 
+            minimumValue={0} maximumValue={50} defaultValue={10}
+            percentageOf={income} 
           />
           <PercentageInput id='investment-slider' label='I want to invest' 
             minimumValue={0} maximumValue={50} defaultValue={10} 
+            percentageOf={income} 
           />
           <PercentageInput id='bills-slider' label='I need to pay bills' 
-            minimumValue={30} maximumValue={80} defaultValue={60} 
+            minimumValue={30} maximumValue={80} defaultValue={60}
+            percentageOf={income}  
           />
           <PercentageInput id='splurge-slider' label='Leftover for splurging' 
             minimumValue={0} maximumValue={30} defaultValue={20} 
+            percentageOf={income} 
           />
         </Grid>
       </Grid>
